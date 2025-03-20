9c57d50c0daae833d2fc87ddf7a2105ee556dc03
mini0192 <parkgw9071@gmail.com>
2025-03-20 11:47:41 +0900

    Fix: memberdetail id 없을 경우 예외 처리

--- src/api/suggest/GetMemerDetailApi.tsx
+++ src/api/suggest/GetMemerDetailApi.tsx
@@ -2,6 +2,9 @@ import { MemberDetailType } from "@/type/MemberType";
 import axios from "axios";
 
 export default async function GetMemerDetailApi(id: string): Promise<MemberDetailType> {
+    if (!id) {
+        throw new Error("ID is required");
+    }
     const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/members/${id}`,
         { withCredentials: true }
     );
