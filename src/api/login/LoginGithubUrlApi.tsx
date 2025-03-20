ae5709dc2fe03e7c947bcaf05c5462bf0de21dba
mini0192 <parkgw9071@gmail.com>
2025-03-20 22:09:43 +0900

    Test: 백엔드 연동 테스트

--- src/api/login/LoginGithubUrlApi.tsx
+++ src/api/login/LoginGithubUrlApi.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export default async function LoginGithubUrlApi(): Promise<string> {
+  console.log(process.env.NEXT_PUBLIC_API_URL);
   const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/oauth/github`);
   return response.data;
 }
