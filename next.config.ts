fc11de4a431ef3d207b806fc5e79afd64448cb55
mini0192 <parkgw9071@gmail.com>
2025-03-20 22:22:19 +0900

    Test: 백엔드 연동 테스트

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      console.log('NEXT_PUBLIC_API_URL:', process.env.NEXT_PUBLIC_API_URL);
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
--- src/api/login/LoginGithubUrlApi.tsx
+++ src/api/login/LoginGithubUrlApi.tsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export default async function LoginGithubUrlApi(): Promise<string> {
-  console.log(process.env.NEXT_PUBLIC_API_URL);
   const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/oauth/github`);
   return response.data;
 }
