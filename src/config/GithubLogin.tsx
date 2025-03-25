128e9aaf9f7dc0523e74a3f306f8e8d126ad1c58
mini0192 <parkgw9071@gmail.com>
2025-03-25 20:19:51 +0900

    Feat: 탈퇴 기능

--- src/config/GithubLogin.tsx
+++ src/config/GithubLogin.tsx
@@ -3,8 +3,8 @@ import LoginGithubUrlApi from "@/api/login/LoginGithubUrlApi";
 export const LoginGithub = () => {
   LoginGithubUrlApi()
     .then((response) => {
-        if (!confirm("GitHub 계정으로 로그인하시겠습니까?")) return;
       if (response.status === 200 && response.data?.url) {
+        if (!confirm("GitHub 계정으로 로그인하시겠습니까?")) return;
         window.location.href = response.data.url;
       }
     })
