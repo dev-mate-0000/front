8a8ac471c852538612d715a2ce7d8a7f1f11b842
mini0192 <parkgw9071@gmail.com>
2025-04-02 14:30:21 +0900

    Fix: 로그인 구현 수정

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,6 +1,22 @@
+"use client";
+
+import getLoginGithubUrl from "@/config/getLoginUrl";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Header() {
+  const [loginUrl, setLoginUrl] = useState("");
+
+  useEffect(() => {
+    getLoginGithubUrl().then((data) => {
+      if (data) {
+        setLoginUrl(data);
+        return;
+      }
+      setLoginUrl("");
+    });
+  }, []);
+
   return (
     <header className="fixed top-0 left-0 right-0 w-full bg-gray-800 shadow-md text-white py-2 z-50">
       <div className="flex justify-between items-center max-w-6xl mx-auto px-4">
@@ -17,12 +33,21 @@ export default function Header() {
           >
             Core
           </Link>
-          <Link
-            href="/my-page"
-            className="text-base hover:text-blue-400 transition duration-300"
-          >
-            MyPage
-          </Link>
+          {loginUrl ? (
+            <Link
+              href={loginUrl}
+              className="text-base hover:text-blue-400 transition duration-300"
+            >
+              Login
+            </Link>
+          ) : (
+            <Link
+              href="/my-page"
+              className="text-base hover:text-blue-400 transition duration-300"
+            >
+              MyPage
+            </Link>
+          )}
         </nav>
       </div>
     </header>
